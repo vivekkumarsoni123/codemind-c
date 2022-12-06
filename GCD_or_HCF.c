cf8c36969aa81d6dbc184722ffb88ee1b9224acb
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2022-12-06 23:36:58 +0530

    #include<stdio.h> int main(){     int n,m;     scanf("%d%d",&n,&m);     int min=n<m?n:m;     for(int i=min;i>=1;i--){         if(n%i==0&&m%i==0){         printf("%d",i);         break;     }     } }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int min=n<m?n:m;
+    for(int i=min;i>=1;i--){
+        if(n%i==0&&m%i==0){
+        printf("%d",i);
+        break;
+    }
+    }
+}
