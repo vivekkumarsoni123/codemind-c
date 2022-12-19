1c4e2e58801f3b696ef4045af030712ad1e7f3b4
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2022-12-19 21:34:57 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int max=a>b?a:b;     int i;     for(i=max;;i++)     {         if(i%a==0 && i%b==0)         {             printf("%d",i);             break;         }     } }

---
+++ LCM_of_two_given_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int max=a>b?a:b;
+    int i;
+    for(i=max;;i++)
+    {
+        if(i%a==0 && i%b==0)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
