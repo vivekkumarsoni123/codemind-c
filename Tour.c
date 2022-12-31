fda8aba18c2d3ac6f4c112323ff229db2b823575
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2022-12-31 09:49:41 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         int m,k;         scanf("%d%d",&m,&k);         printf("%d ",m*5+k*7);     } }

---
+++ Tour.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        int m,k;
+        scanf("%d%d",&m,&k);
+        printf("%d
+",m*5+k*7);
+    }
+}
