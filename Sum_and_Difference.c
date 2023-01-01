1902fe1565f628465d1abe5b906ec06454789764
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-01-01 22:57:57 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     float k,j;     scanf("%f%f",&k,&j);     printf("%d %d ",n+m,n-m);     printf("%.1f%1f",k+j,k-j); }

---
+++ Sum_and_Difference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    float k,j;
+    scanf("%f%f",&k,&j);
+    printf("%d %d
+",n+m,n-m);
+    printf("%.1f%1f",k+j,k-j);
+}
