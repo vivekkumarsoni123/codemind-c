8d654ded45f7b31ef62b8a1eee45d44d61de18c2
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-01-01 23:09:09 +0530

    #include<stdio.h> int main() {     int n;     float s;     scanf("%d",&n);     if(n<200) s=n*1.20;     else if(n>=200&&n<400) s=n*1.50;     else if(n>=400&&n<600) s=n*1.80;     else s=n*2.00;     if(s<=400)printf("%.2f",s+100);     else printf("%.2f",s+(.15*s)); }

--- Current_bill.c
+++ Current_bill.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,k,s;
-    scanf("%d%d%d",&n,&m,&k);
+    int n;
+    float s;
+    scanf("%d",&n);
     if(n<200) s=n*1.20;
-    else if(n>=200&&n<400)s=n*1.50;
-    else if(n>=400&&n<600)s=n*1.80;
-    else if(n>=600)s=n*2.00;
-    if(s>=400)printf("%.2f",(float)s+0.15*s);
-    else if(s<400)printf("%.2f",(float)s+100);
+    else if(n>=200&&n<400) s=n*1.50;
+    else if(n>=400&&n<600) s=n*1.80;
+    else s=n*2.00;
+    if(s<=400)printf("%.2f",s+100);
+    else printf("%.2f",s+(.15*s));
 }
