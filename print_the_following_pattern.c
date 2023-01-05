498ed63855295bfc9746c99e85d7ea4ddf6e1fbb
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-01-05 18:47:36 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j||j==1||j==n)printf("* ");             else printf("  ");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,12 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=65+(n-1);i>=65;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=65;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%c ",i);
+            if(i==j||j==1||j==n)printf("* ");
+            else printf("  ");
         }
         printf("
 ");
