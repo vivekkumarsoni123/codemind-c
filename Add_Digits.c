89d26e488ecca3eb5a7dd24e892ace1f5239dfa4
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-05-19 14:47:55 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n>9) {         int rem = n%10;         n = n / 10;         n = n + rem;     }     printf("%d",n); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-int main()
-{
-    int n,s=0,i,r;
+int main() {
+    int n;
     scanf("%d",&n);
-    while(n>9)
-    {
-        r=n%10;
-        n=n/10;
-        n=n+r;
+    while(n>9) {
+        int rem = n%10;
+        n = n / 10;
+        n = n + rem;
     }
     printf("%d",n);
 }
