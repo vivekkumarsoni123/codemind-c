25d7e8a42960a724a41d2f6097d928fd5bac3a46
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-06-06 16:12:23 +0530

    #include<stdio.h> int pal(int n){     int s=0,q=n;     while(n){         int r=n%10;         s=s*10+r;         n=n/10;     }     if(s==q) return 1;     else return 0; } int main(){     int n,p1,p2,d1=0,d2=0;     scanf("%d",&n);     for(int i=n+1;;i++){         d1++;         if(pal(i)){             p1=i;             break;         }     }     for(int i=n-1;;i--){         d2++;         if(pal(i)){             p2=i;             break;         }     }     if(d1 < d2) printf("%d",p1);     else if(d2 < d1) printf("%d",p2);     else printf("%d %d",p2,p1); }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,40 +1,32 @@
 #include<stdio.h>
-int pal(int n)
-{
-    int r,s=0,p;
-    p=n;
-    while(n!=0)
-    {
-        r=n%10;
+int pal(int n){
+    int s=0,q=n;
+    while(n){
+        int r=n%10;
         s=s*10+r;
         n=n/10;
     }
-    if(s==p)return 1;
+    if(s==q) return 1;
     else return 0;
 }
-int main()
-{
-    int n,i,d1=0,d2=0,np1=0,np2=0;
+int main(){
+    int n,p1,p2,d1=0,d2=0;
     scanf("%d",&n);
-    for(i=n+1;;i++)
-    {
-        if(pal(i))
-        {
-            np1=i;
-            d1=i-n;
+    for(int i=n+1;;i++){
+        d1++;
+        if(pal(i)){
+            p1=i;
             break;
         }
     }
-    for(i=n-1;;i--)
-    {
-        if(pal(i))
-        {
-            np2=i;
-            d2=n-i;
+    for(int i=n-1;;i--){
+        d2++;
+        if(pal(i)){
+            p2=i;
             break;
         }
     }
-    if(d1<d2)printf("%d",np1);
-    else if(d2<d1)printf("%d",np2);
-    else printf("%d %d",np2,np1);
+    if(d1 < d2) printf("%d",p1);
+    else if(d2 < d1) printf("%d",p2);
+    else printf("%d %d",p2,p1);
 }
