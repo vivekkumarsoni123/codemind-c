30ac629c4029fb447a6cd0f6beb07c5a5848941e
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-06-06 16:51:38 +0530

    #include<stdio.h> int prime(int n){     if(n==1) return 0;     else {         int c=0;         for(int i=2;i<=n/2;i++){             if(n%i==0){                 c=1;                 break;             }         }         if(c==0) return 1;         else return 0;     } } int main(){     int t;     scanf("%d",&t);     if(prime(t)){         while(t){             int r=t%10;             if(prime(r)){                 t=t/10;             }             else break;         }         if(t==0) printf("Mega Prime");         else printf("Not Mega Prime");     }     else printf("Not Mega Prime"); }

--- mega_prime.c
+++ mega_prime.c
@@ -1,31 +1,30 @@
 #include<stdio.h>
-int prime(int n)
-{
-    int i,c=0;
-    for(i=1;i<=n;i++)
-    {
-        if(n%i==0)
-        c++;
+int prime(int n){
+    if(n==1) return 0;
+    else {
+        int c=0;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                c=1;
+                break;
+            }
+        }
+        if(c==0) return 1;
+        else return 0;
     }
-    if(c==2)return 1;
-    else return 0;
 }
-int main()
-{
-    int n,r,c=0,ct=0;
-    scanf("%d",&n);
-    if(prime(n))
-    {
-    while(n!=0)
-    {
-        c++;
-        r=n%10;
-        if(prime(r))
-            ct++;
-        n=n/10;
-    }
-        if(c==ct)
-        printf("Mega Prime");
+int main(){
+    int t;
+    scanf("%d",&t);
+    if(prime(t)){
+        while(t){
+            int r=t%10;
+            if(prime(r)){
+                t=t/10;
+            }
+            else break;
+        }
+        if(t==0) printf("Mega Prime");
         else printf("Not Mega Prime");
     }
     else printf("Not Mega Prime");
