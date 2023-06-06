9210537efc59ebb30d18d637677b53d5f3a2eac5
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-06-06 16:28:35 +0530

    #include<stdio.h> int prime(int n){     int c=0;     for(int i=2;i<=n/2;i++){         if(n%i==0){             c=1;             break;         }     }     if(c==0) return 1;     else return 0; } int main(){     int t;     scanf("%d",&t);     while(t--){         int n,d1=0,d2=0,p1,p2;         scanf("%d",&n);         for(int i=n;;i++){             d1++;             if(prime(i)){                 p1=i;                 break;             }         }         for(int i=n;;i--){             d2++;             if(prime(i)){                 p2=i;                 break;             }         }         if(d1>d2) printf("%d ",p2);         else if(d1<d2) printf("%d ",p1);         else printf("%d ",p2);     } }

--- Nearest_Prime.c
+++ Nearest_Prime.c
@@ -1,49 +1,40 @@
-// Online C compiler to run C program onli
 #include<stdio.h>
-int prime(int n)
-{
-    int d=0,i;
-    for(i=2;i<=n;i++)
-    {
-        if(n%i==0)
-        {
-            d++;
+int prime(int n){
+    int c=0;
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            c=1;
+            break;
         }
-        
     }
-    if(d==1)return 1;
-        else return 0;
+    if(c==0) return 1;
+    else return 0;
 }
-int main()
-{
-    int k,i,n,d1=0,d2=0,np1=0,np2=0,t;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    {
-        scanf("%d",&t);
-        for(k=t;;k++)
-        {
-            if(prime(k))
-            {
-                d1=k-t;
-                np1=k;
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int n,d1=0,d2=0,p1,p2;
+        scanf("%d",&n);
+        for(int i=n;;i++){
+            d1++;
+            if(prime(i)){
+                p1=i;
                 break;
             }
         }
-        for(k=t-1;;k--)
-        {
-            if(prime(k))
-            {
-                d2=t-k;
-                np2=k;
+        for(int i=n;;i--){
+            d2++;
+            if(prime(i)){
+                p2=i;
                 break;
             }
         }
-        if(d1<d2)printf("%d
-",np1);
-        else if(d2<d1)printf("%d
-",np2);
+        if(d1>d2) printf("%d
+",p2);
+        else if(d1<d2) printf("%d
+",p1);
         else printf("%d
-",np2);
+",p2);
     }
 }
