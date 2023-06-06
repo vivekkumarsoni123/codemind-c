531a53ca54a32078fe86a58b480cef36a8eaa958
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-06-07 00:11:07 +0530

    #include<stdio.h> int main(){     int n,k;     scanf("%d",&n);     int x[n];     for(int i=0;i<n;i++){         scanf("%d",&x[i]);         if(x[i]%2 != 0 ) k=x[i];     }     printf("%d",k); }

---
+++ Last_odd_number_in_an_array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,k;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&x[i]);
+        if(x[i]%2 != 0 ) k=x[i];
+    }
+    printf("%d",k);
+}
