ba54a71a8c7945e5603b9abf7b685170a38efb60
vivekkumarsoni123 <119720668+vivekkumarsoni123@users.noreply.github.com>
2023-06-08 23:12:15 +0530

    #include<stdio.h> int main(){     int n,b[1000],k=0;     scanf("%d",&n);     int x[n];     for(int i=0;i<n;i++){         scanf("%d",&x[i]);         if(x[i]%2 == 0) printf("%d ",x[i]);         else b[k++] = x[i];     }     for(int i=0;i<k;i++){         printf("%d ",b[i]);     } }

---
+++ Even_elements_followed_by_odd_elements.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n,b[1000],k=0;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&x[i]);
+        if(x[i]%2 == 0) printf("%d ",x[i]);
+        else b[k++] = x[i];
+    }
+    for(int i=0;i<k;i++){
+        printf("%d ",b[i]);
+    }
+}
